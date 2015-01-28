c2b0f62fa0b3b9e3c5ce8750415c0f12198e5fe9
LoganArnett <Loganarn5@gmail.com>
2015-01-28 12:43:46 -0500

    adding inappbrowser back in

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -15,17 +15,17 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
-        "id": "org.apache.cordova.inappbrowser.inappbrowser",
-        "clobbers": [
-            "window.open"
-        ]
-    },
-    {
         "file": "plugins/com.bez4pieci.cookies/www/cookies.js",
         "id": "com.bez4pieci.cookies.cookies",
         "clobbers": [
             "cookies"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
         ]
     }
 ];
@@ -34,8 +34,8 @@ module.exports.metadata =
 {
     "com.ionic.keyboard": "1.0.3",
     "org.apache.cordova.device": "0.2.13",
-    "org.apache.cordova.inappbrowser": "0.5.4",
-    "com.bez4pieci.cookies": "0.0.1"
+    "com.bez4pieci.cookies": "0.0.1",
+    "org.apache.cordova.inappbrowser": "0.5.4"
 }
 // BOTTOM OF METADATA
 });
